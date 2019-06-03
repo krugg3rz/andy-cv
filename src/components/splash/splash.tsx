c96235352e81a56116c2809973f2a9e40878e797
krugg3rz <krugg3rz@gmail.com>
2019-06-03 18:44:21 +0100

    Adjusted splash positioning & added profile image

--- src/components/splash/splash.tsx
+++ src/components/splash/splash.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styles from "../splash/splash.module.scss";
+import splashImage from "../../assets/images/splashBackground.png"
 
 export interface IProps {
     
@@ -15,7 +16,7 @@ class Splash extends React.Component<IProps, IState> {
             <article>
                 <section>
                     <h1 className={styles.name}>Andy Ford</h1>
-                    <h2>Front-end Developer</h2>
+                    <h2 className={styles.role}>Front-end Developer</h2>
                 </section>
             </article>
          );
--- src/containers/splashContainer/splashContainer.tsx
+++ src/containers/splashContainer/splashContainer.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import Splash from "../../components/splash/splash";
+import SplashImage from "../../assets/images/splashbackground.png";
+import styles from "./splashContainer.module.scss";
 
 export interface IProps {
     
@@ -12,7 +14,10 @@ export interface IState {
 class SplashContainer extends React.Component<IProps, IState> {
     render() { 
         return ( 
+            <section>
             <Splash />
+            <img src={SplashImage} className={styles.image}></img>
+            </section>
          );
     }
 }
